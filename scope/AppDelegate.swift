99b42ecd3bad04d1ac227dc8b5fc65552e086db5
handlemotion <jimenezzr.05@gmail.com>
2025-09-16 12:09:45 -0600

    Initial Commit

---
+++ scope/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  scope
+//
+//  Created by Rodrigo Jiménez on 16/09/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ scopeTests/scopeTests.swift
@@ -0,0 +1,17 @@
+//
+//  scopeTests.swift
+//  scopeTests
+//
+//  Created by Rodrigo Jiménez on 16/09/25.
+//
+
+import Testing
+@testable import scope
+
+struct scopeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
